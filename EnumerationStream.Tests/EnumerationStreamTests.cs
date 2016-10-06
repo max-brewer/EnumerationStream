abc8c8f6dc104889370c7d9a273dee5882816bda
Max Brewer <max.brewer@acumenci.com>
2016-10-06 23:47:45 +0100

    Started the basic outline of the enumeration stream. Still need to allow different serialization strategies. It might be better implemented as a streamWriter. Still need to read an enumeration from a stream.

---
+++ EnumerationStream.Tests/EnumerationStreamTests.cs
@@ -0,0 +1,80 @@
+﻿using System.IO;
+using Xunit;
+
+namespace EnumerationStream.Tests
+{
+    public class EnumerationStreamTests
+    {
+        [Fact]
+        public void It_is_a_Stream()
+        {
+            using (var target = GetTarget())
+                Assert.IsAssignableFrom<Stream>(target);
+        }
+
+        [Fact]
+        public void It_is_not_Writeable()
+        {
+            using (var target = GetTarget())
+                Assert.False(target.CanWrite);
+        }
+
+        [Fact]
+        public void It_is_not_Seekable()
+        {
+            using (var target = GetTarget())
+                Assert.False(target.CanSeek);
+        }
+
+        [Fact]
+        public void It_is_Readable()
+        {
+            using (var target = GetTarget())
+                Assert.True(target.CanRead);
+        }
+
+        [Fact]
+        public void An_empty_enumeration_streams_an_empty_Json_array()
+        {
+            Assert.Equal("[]", ReadTarget());
+        }
+
+        [Fact]
+        public void An_enumeration_of_one_object_streams_its_Json_representation_in_an_array()
+        {
+            Assert.Equal("[{}]", ReadTarget(new object()));
+        }
+
+        [Fact]
+        public void An_enumeration_of_two_object_streams_their_Json_representations_in_an_array()
+        {
+            Assert.Equal("[{},{}]", ReadTarget(new object(),new object()));
+        }
+
+        [Fact]
+        public void Correctly_serialises_simple_object()
+        {
+            Assert.Equal("[{\"number\":1,\"text\":\"Some Text\"}]", 
+                ReadTarget(new {Number=1, Text = "Some Text"}));
+        }
+
+        [Fact]
+        public void Correctly_handles_non_ascii_characters()
+        {
+            Assert.Equal("[{\"turkishEye\":\"Iıİi\"},{\"kanji\":\"漢字\"}]",
+                ReadTarget(new {TurkishEye = "Iıİi"}, new { Kanji = "漢字"}));
+        }
+
+        private static string ReadTarget(params object[] contents)
+        {
+            using (var target = GetTarget(contents))
+            using (var reader = new StreamReader(target))
+                return reader.ReadToEnd();
+        }
+
+        private static EnumerationStream GetTarget(params object[] contents)
+        {
+            return new EnumerationStream(contents);
+        }
+    }
+}
---
+++ EnumerationStream/EnumerationStream.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace EnumerationStream
+{
+    public class EnumerationStream : Stream
+    {
+        private static readonly JsonSerializerSettings JsonSerializerSettings = new JsonSerializerSettings
+        {
+            ContractResolver = new CamelCasePropertyNamesContractResolver()
+        };
+
+        private readonly IEnumerator<byte> _byteEnumerator;
+
+        public EnumerationStream(IEnumerable enumerable)
+        {
+            _byteEnumerator = GetBytes(enumerable).GetEnumerator();
+        }
+
+        public override void Flush()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SetLength(long value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            var position = offset;
+            for (; position < buffer.Length && _byteEnumerator.MoveNext(); position++)
+                buffer[position] = _byteEnumerator.Current;
+
+            return position - offset;
+        }
+
+        private static IEnumerable<byte> GetBytes(IEnumerable enumerable)
+        {
+            return HeadBytes
+                .Concat(GetContentBytes(enumerable))
+                .Concat(TailBytes);
+        }
+
+        private static IEnumerable<byte> GetContentBytes(IEnumerable enumerable)
+        {
+            var contentEnumerator = enumerable.GetEnumerator();
+
+            if (!contentEnumerator.MoveNext())
+                yield break;
+
+            foreach (var b in GetBytes(contentEnumerator.Current))
+                yield return b;
+
+
+            while (contentEnumerator.MoveNext())
+            {
+                foreach (var b in GetBytes(","))
+                    yield return b;
+
+                foreach (var b in GetBytes(contentEnumerator.Current))
+                    yield return b;
+            }
+        }
+
+
+        private static IEnumerable<byte> HeadBytes => GetBytes("[");
+        private static IEnumerable<byte> TailBytes => GetBytes("]");
+
+        private static IEnumerable<byte> GetBytes(object item) =>
+            GetBytes(JsonConvert.SerializeObject(item, JsonSerializerSettings));
+
+        private static IEnumerable<byte> GetBytes(string characters) =>
+            System.Text.Encoding.UTF8.GetBytes(characters);
+
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool CanRead => true;
+        public override bool CanSeek => false;
+        public override bool CanWrite => false;
+        public override long Length { get; }
+        public override long Position { get; set; }
+    }
+}
